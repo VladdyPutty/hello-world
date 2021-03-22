3599dabe0bd14f2f037afa4709a60da8c452db2f
VladdyPutty <81063729+VladdyPutty@users.noreply.github.com>
2021-03-21 23:09:28 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name
+print('Hello World!')
+print('What is your name?') #ask for the name
+myName = input()
+print('It is good to meet you, ' + myName + '!')
+print('The length of you name is:')
+print(str(len(myName)) + ' characters long')
+print('What is your age?') # asks for age
+myAge = input()
+print('You will be ' + str(int(myAge)+1) + ' in a year.')
